2d70104f2bd9644c3a3348ccf546dcbe097b231f
priyanshmalakar <91202215+priyanshmalakar@users.noreply.github.com>
2024-04-25 13:27:47 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,25 +1,21 @@
 function smallestMissingPositiveInteger(nums) {
   const n = nums.length;
 
-  // Mark elements that are within the range of 1 to n
   for (let i = 0; i < n; i++) {
       if (nums[i] > 0 && nums[i] <= n) {
           const index = nums[i] - 1;
-          // Mark presence by making the element negative
           if (nums[index] > 0) {
               nums[index] = -nums[index];
           }
       }
   }
 
-  // Find the smallest missing positive integer
   for (let i = 0; i < n; i++) {
       if (nums[i] > 0) {
           return i + 1;
       }
   }
 
-  // If all elements are negative, return n + 1
   return n + 1;
 }
 
