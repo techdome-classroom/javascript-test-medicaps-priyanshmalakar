7fc466dfa6df827790089580127885e5e378f3ac
priyanshmalakar <91202215+priyanshmalakar@users.noreply.github.com>
2024-04-25 14:00:58 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,7 +1,18 @@
+function smallestMissingPositiveInteger(nums) {
+  nums = nums.filter(num => num > 0).sort((a, b) => a - b);
 
-function smallestMissingPositiveInteger(_nums) {
+  if (nums[0] !== 1) {
+    return 1;
+  }
 
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i + 1] - nums[i] > 1) {
+      return nums[i] + 1;
+    }
+  }
 
+  return nums[nums.length - 1] + 1;
 }
+
 
 module.exports = smallestMissingPositiveInteger;
