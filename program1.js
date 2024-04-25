0cd5dcb730261ecc6e0f8f1a39149c363147b1b8
priyanshmalakar <91202215+priyanshmalakar@users.noreply.github.com>
2024-04-25 13:20:53 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -3,7 +3,14 @@ function longestSubstring(s) {
     let start = 0;
     let charIndexMap = {};
 
- 
+    for (let end = 0; end < s.length; end++) {
+        const currentChar = s[end];
+        if (charIndexMap[currentChar] >= start) {
+            start = charIndexMap[currentChar] + 1;
+        }
+        charIndexMap[currentChar] = end;
+        maxLength = Math.max(maxLength, end - start + 1);
+    }
 
     return maxLength;
 }
