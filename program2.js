8208ede447f3f371b50d1c8372cd39ed6bc18853
priyanshmalakar <91202215+priyanshmalakar@users.noreply.github.com>
2024-04-25 13:31:21 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,21 +1,7 @@
-function smallest_missing_positive_integer(nums) {
-  // Create a set to store positive integers
-  const numSet = new Set();
+function smallestMissingPositiveInteger(nums) {
 
-  // Add positive integers to the set
-  for (const num of nums) {
-      if (num > 0) {
-          numSet.add(num);
-      }
-  }
+  // Implement the function smallest_missing_positive_integer
 
-
-  let smallestMissing = 1;
-  while (numSet.has(smallestMissing)) {
-      smallestMissing++;
-  }
-
-  return smallestMissing;
 }
 
-module.exports = smallest_missing_positive_integer;
+module.exports = smallestMissingPositiveInteger;
