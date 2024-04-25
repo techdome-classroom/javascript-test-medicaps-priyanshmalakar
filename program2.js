45e07f80e7403df839254335ceacb1a39e71d271
priyanshmalakar <91202215+priyanshmalakar@users.noreply.github.com>
2024-04-25 13:30:58 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,25 +1,21 @@
 function smallest_missing_positive_integer(nums) {
-  // Remove non-positive integers and duplicates
-  nums = Array.from(new Set(nums.filter(num => num > 0)));
+  // Create a set to store positive integers
+  const numSet = new Set();
 
-  // If nums is empty, return 1
-  if (nums.length === 0) return 1;
-
-  // Sort the array
-  nums.sort((a, b) => a - b);
-
-  // If the first element is not 1, return 1
-  if (nums[0] !== 1) return 1;
-
-  // Find the smallest missing positive integer
-  for (let i = 0; i < nums.length - 1; i++) {
-      if (nums[i + 1] - nums[i] > 1) {
-          return nums[i] + 1;
+  // Add positive integers to the set
+  for (const num of nums) {
+      if (num > 0) {
+          numSet.add(num);
       }
   }
 
-  // If all integers from 1 to the maximum in nums are present, return the next integer
-  return nums[nums.length - 1] + 1;
+  // Find the smallest missing positive integer
+  let smallestMissing = 1;
+  while (numSet.has(smallestMissing)) {
+      smallestMissing++;
+  }
+
+  return smallestMissing;
 }
 
 module.exports = smallest_missing_positive_integer;
