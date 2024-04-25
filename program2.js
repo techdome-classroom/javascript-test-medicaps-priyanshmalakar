08741aa1bb89c9014c43e672ef32ac3a9f3a6357
priyanshmalakar <91202215+priyanshmalakar@users.noreply.github.com>
2024-04-25 13:47:20 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,9 +1,11 @@
+
+
 function smallestMissingPositiveInteger(nums) {
 
   // Implement the function smallest_missing_positive_integer
 let newNums = nums.filter((num) => num > 0);
 let min = 1; 
- 
+
 
 }
 
